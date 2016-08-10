678106f52ca0282835e460b45bc2ebaffe30fd36
Ripan <er.ripankumar@gmail.com>
2016-08-10 22:46:35 +0100

    Add Spec

--- spec/ink_spec.rb
+++ spec/ink_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/teespring/ink'
 
 describe Ink do
- 
-
+  it "should have color"
+  it "should have cost per unit volume"
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,6 +1,11 @@
 require_relative '../lib/teespring/question'
 
 describe Question do
- 
+
+  it "should have number of layers"
+  context "layers" do
+    it "should have color"
+    it "should have volume"
+  end
 
 end
